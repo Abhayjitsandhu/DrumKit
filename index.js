5567e1f349771317b0ad542f3f503d49d2062158
Abhayjit Singh <abhayjitsandhu966@gmail.com>
2020-10-08 23:43:33 +0530

    Drum Kit Complete

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
+// detectin button press
 var i=document.querySelectorAll(".drum").length-1;
 while(i>=0)
 {
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-  alert("I got clicked");
+
+  var buttonInnerHtml= this.innerHTML;
+  makeSound(buttonInnerHtml);
+  buttonAnimation(buttonInnerHtml);
 });
 i--;
+}
+
+//detection keyboard press
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key)
+{
+  switch (key) {
+    case 'w':
+      var audio= new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+    case 'a':
+      var audio= new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case 's':
+      var audio= new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+    case 'd':
+      var audio= new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case 'j':
+      var audio= new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 'k':
+      var audio= new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'l':
+      var audio= new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    default:
+    console.log(key);
+
+  }
+}
+
+function buttonAnimation(currentKey)
+{
+  var activeButton=document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  },100);
 }
